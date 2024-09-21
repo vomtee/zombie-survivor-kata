1183d6482d5c6f73550ba4f380dd333ac7ddb424
vomtee <v85ngm@gmail.com>
2024-09-21 21:24:47 +0200

    Put duplicated code into EquipmentList

--- src/main/kotlin/zombiegame/Equipment.kt
+++ src/main/kotlin/zombiegame/Equipment.kt
@@ -19,33 +19,32 @@ interface EquipmentHolder {
     fun addItemOperation(item: EquipmentType): Boolean
 }
 
-class InHands : EquipmentHolder {
-    private val items = mutableListOf<EquipmentType>()
-    override val amount: Int
-        get() = items.size
-    override val maxAmount: Int
-        get() = 2
-
-    override fun addItemOperation(item: EquipmentType): Boolean {
-        return items.add(item)
-    }
-}
-
-class InReserve : EquipmentHolder {
-    private val items = mutableListOf<EquipmentType>()
-    override val amount: Int
-        get() = items.size
-    override val maxAmount: Int
-        get() = 3
-
-    override fun addItemOperation(item: EquipmentType): Boolean {
-        return items.add(item)
-    }
-}
-
 fun EquipmentHolder.add(item: EquipmentType): Boolean =
     if (amount < maxAmount) addItemOperation(item) else false
 
+abstract class EquipmentList : EquipmentHolder {
+    private val items = mutableListOf<EquipmentType>()
+    override val amount: Int
+        get() = items.size
+
+    override fun addItemOperation(item: EquipmentType): Boolean {
+        return items.add(item)
+    }
+}
+
+private const val MAX_IN_HAND_AMOUNT = 2
+
+class InHands : EquipmentList() {
+    override val maxAmount: Int
+        get() = MAX_IN_HAND_AMOUNT
+}
+
+private const val MAX_IN_RESERVE_AMOUNT = 3
+
+class InReserve : EquipmentList() {
+    override val maxAmount: Int
+        get() = MAX_IN_RESERVE_AMOUNT
+}
 
 enum class EquipmentType {
     ANYTHING,

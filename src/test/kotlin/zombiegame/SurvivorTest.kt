f310645c43f76876f5a37f22a43052768ab2901e
vomtee <v85ngm@gmail.com>
2024-09-21 21:48:28 +0200

    Test where specific items are

--- src/test/kotlin/zombiegame/SurvivorTest.kt
+++ src/test/kotlin/zombiegame/SurvivorTest.kt
@@ -1,12 +1,18 @@
 package zombiegame
 
 import Survivor
+import io.kotest.matchers.equals.shouldBeEqual
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNot
 import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.string.beEmpty
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import zombiegame.EquipmentType.BASEBALL_BAT
+import zombiegame.EquipmentType.BOTTLED_WATER
+import zombiegame.EquipmentType.FRYING_PAN
+import zombiegame.EquipmentType.KATANA
+import zombiegame.EquipmentType.PISTOL
 
 
 class SurvivorTest {
@@ -94,7 +100,7 @@ class SurvivorTest {
     @Test
     fun `survivor can carry 5 items` () {
         repeat(5) {
-            survivor.equipment.add(EquipmentType.BASEBALL_BAT)
+            survivor.equipment.add(BASEBALL_BAT)
         }
         survivor.equipment.amount shouldBe 5
     }
@@ -102,7 +108,7 @@ class SurvivorTest {
     @Test
     fun `survivor can carry not more than 5 items` () {
         repeat(6) {
-            survivor.equipment.add(EquipmentType.BASEBALL_BAT)
+            survivor.equipment.add(BASEBALL_BAT)
         }
         survivor.equipment.amount shouldBe 5
     }
@@ -110,12 +116,25 @@ class SurvivorTest {
     @Test
     fun `survivor carries 2 items in hand and 3 items in reserve` () {
         repeat(5) {
-            survivor.equipment.add(EquipmentType.BASEBALL_BAT)
+            survivor.equipment.add(BASEBALL_BAT)
         }
 
         survivor.equipment.amount shouldBe 5
         survivor.equipment.inHands.amount shouldBe 2
         survivor.equipment.inReserve.amount shouldBe 3
+    }
+
+    @Test
+    fun `survivor carries 2 specifc items in hand and 3 specific items in reserve` () {
+        survivor.equipment.add(BASEBALL_BAT)
+        survivor.equipment.add(FRYING_PAN)
+        survivor.equipment.add(KATANA)
+        survivor.equipment.add(PISTOL)
+        survivor.equipment.add(BOTTLED_WATER)
+
+        survivor.equipment.inHands.content shouldBeEqual listOf(BASEBALL_BAT, FRYING_PAN)
+        survivor.equipment.inReserve.content shouldBeEqual listOf(KATANA, PISTOL, BOTTLED_WATER)
+
     }
 
 }

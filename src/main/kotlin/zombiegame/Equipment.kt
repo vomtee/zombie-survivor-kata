46c28b59d415f74d65d58aa2a8603ebb7b67e4ed
vomtee <v85ngm@gmail.com>
2024-09-23 19:56:57 +0200

    Reorder code in Equipment.kt

--- src/main/kotlin/zombiegame/Equipment.kt
+++ src/main/kotlin/zombiegame/Equipment.kt
@@ -1,19 +1,15 @@
 package zombiegame
 
-class Equipment : EquipmentHolder {
-    val inHands = InHands()
-    val inReserve = InReserve()
-    override val amount: Int
-        get() = this.inHands.amount + this.inReserve.amount
+private const val MAX_IN_HAND_AMOUNT = 2
+private const val MAX_IN_RESERVE_AMOUNT = 3
 
-    override val maxAmount: Int
-        get() = this.inHands.maxAmount + this.inReserve.maxAmount
-
-    override val content: List<EquipmentType>
-        get() = inHands.content + inReserve.content
-
-    override fun addItemOperation(item: EquipmentType): Boolean =
-        if (inHands.add(item)) true else inReserve.add(item)
+enum class EquipmentType {
+    BASEBALL_BAT,
+    FRYING_PAN,
+    KATANA,
+    PISTOL,
+    BOTTLED_WATER,
+    MOLOTOV,
 }
 
 interface EquipmentHolder {
@@ -26,35 +22,46 @@ interface EquipmentHolder {
 fun EquipmentHolder.add(item: EquipmentType): Boolean =
     if (amount < maxAmount) addItemOperation(item) else false
 
+class Equipment : EquipmentHolder {
+    val inHands = InHands()
+    val inReserve = InReserve()
+
+    override val amount: Int
+        get() = this.inHands.amount + this.inReserve.amount
+
+    override val maxAmount: Int
+        get() = this.inHands.maxAmount + this.inReserve.maxAmount
+
+    override val content: List<EquipmentType>
+        get() = inHands.content + inReserve.content
+
+    override fun addItemOperation(item: EquipmentType): Boolean =
+        if (inHands.add(item))
+            true
+        else
+            inReserve.add(item)
+}
+
 abstract class EquipmentList : EquipmentHolder {
     private val items = mutableListOf<EquipmentType>()
+
     override val amount: Int
         get() = items.size
+
     override val content: List<EquipmentType>
         get() = items.toList()
 
-    override fun addItemOperation(item: EquipmentType): Boolean {
-        return items.add(item)
-    }
+    override fun addItemOperation(item: EquipmentType): Boolean =
+        items.add(item)
 }
 
-private const val MAX_IN_HAND_AMOUNT = 2
 class InHands : EquipmentList() {
     override val maxAmount: Int
         get() = MAX_IN_HAND_AMOUNT
 }
 
-private const val MAX_IN_RESERVE_AMOUNT = 3
 class InReserve : EquipmentList() {
     override val maxAmount: Int
         get() = MAX_IN_RESERVE_AMOUNT
 }
 
-enum class EquipmentType {
-    BASEBALL_BAT,
-    FRYING_PAN,
-    KATANA,
-    PISTOL,
-    BOTTLED_WATER,
-    MOLOTOV,
-}

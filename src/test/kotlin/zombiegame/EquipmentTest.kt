2162cc89c0579474ed1c08e45a0584d2c2fac1d4
vomtee <v85ngm@gmail.com>
2024-09-23 19:49:55 +0200

    Isolate equipment tests

---
+++ src/test/kotlin/zombiegame/EquipmentTest.kt
@@ -0,0 +1,59 @@
+package zombiegame
+
+import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import zombiegame.EquipmentType.BASEBALL_BAT
+import zombiegame.EquipmentType.BOTTLED_WATER
+import zombiegame.EquipmentType.FRYING_PAN
+import zombiegame.EquipmentType.KATANA
+import zombiegame.EquipmentType.PISTOL
+
+class EquipmentTest {
+    val equipment = Equipment()
+    
+    @Test
+    fun `equipment initially is empty nothing` () {
+        equipment.amount shouldBe 0
+    }
+
+    @Test
+    fun `equipment can hold 5 items` () {
+        repeat(5) {
+            equipment.add(BASEBALL_BAT)
+        }
+        equipment.amount shouldBe 5
+    }
+
+    @Test
+    fun `equipment can hold not more than 5 items` () {
+        repeat(6) {
+            equipment.add(BASEBALL_BAT)
+        }
+        equipment.amount shouldBe 5
+    }
+
+    @Test
+    fun `equipment holds 2 items in hand and 3 items in reserve` () {
+        repeat(5) {
+            equipment.add(BASEBALL_BAT)
+        }
+
+        equipment.inHands.amount shouldBe 2
+        equipment.inReserve.amount shouldBe 3
+    }
+
+    @Test
+    fun `equipment holds 2 specific items in hand and 3 specific items in reserve` () {
+        equipment.add(BASEBALL_BAT)
+        equipment.add(FRYING_PAN)
+        equipment.add(KATANA)
+        equipment.add(PISTOL)
+        equipment.add(BOTTLED_WATER)
+
+        equipment.inHands.content shouldBeEqual listOf(BASEBALL_BAT, FRYING_PAN)
+        equipment.inReserve.content shouldBeEqual listOf(KATANA, PISTOL, BOTTLED_WATER)
+
+    }
+
+}
--- src/test/kotlin/zombiegame/SurvivorTest.kt
+++ src/test/kotlin/zombiegame/SurvivorTest.kt
@@ -1,27 +1,14 @@
 package zombiegame
 
 import Survivor
-import io.kotest.matchers.equals.shouldBeEqual
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNot
 import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.string.beEmpty
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import zombiegame.EquipmentType.BASEBALL_BAT
-import zombiegame.EquipmentType.BOTTLED_WATER
-import zombiegame.EquipmentType.FRYING_PAN
-import zombiegame.EquipmentType.KATANA
-import zombiegame.EquipmentType.PISTOL
-
 
 class SurvivorTest {
-    lateinit var survivor: Survivor
-
-    @BeforeEach
-    fun setup() {
-        survivor = Survivor(" Heinz")
-    }
+    private val survivor: Survivor = Survivor("Local Horst")
 
     @Test
     fun `survivor has a name`() {
@@ -73,68 +60,23 @@ class SurvivorTest {
 
     @Test
     fun `survivor cannot perform more than 3 actions`() {
-        survivor.act()
-        survivor.act()
-        survivor.act()
+        repeat(3) {
+            survivor.act()
+        }
 
         survivor.act() shouldBe false
     }
 
     @Test
     fun `survivor should be able to perform actions again with the next turn`() {
-        survivor.act()
-        survivor.act()
-        survivor.act()
+        repeat(3) {
+            survivor.act()
+        }
+
         survivor.act() shouldBe false
 
         survivor.nextTurn()
         survivor.act() shouldBe true
 
     }
-
-    @Test
-    fun `survivor initially carries nothing` () {
-        survivor.equipment.amount shouldBe 0
-    }
-
-    @Test
-    fun `survivor can carry 5 items` () {
-        repeat(5) {
-            survivor.equipment.add(BASEBALL_BAT)
-        }
-        survivor.equipment.amount shouldBe 5
-    }
-
-    @Test
-    fun `survivor can carry not more than 5 items` () {
-        repeat(6) {
-            survivor.equipment.add(BASEBALL_BAT)
-        }
-        survivor.equipment.amount shouldBe 5
-    }
-
-    @Test
-    fun `survivor carries 2 items in hand and 3 items in reserve` () {
-        repeat(5) {
-            survivor.equipment.add(BASEBALL_BAT)
-        }
-
-        survivor.equipment.amount shouldBe 5
-        survivor.equipment.inHands.amount shouldBe 2
-        survivor.equipment.inReserve.amount shouldBe 3
-    }
-
-    @Test
-    fun `survivor carries 2 specifc items in hand and 3 specific items in reserve` () {
-        survivor.equipment.add(BASEBALL_BAT)
-        survivor.equipment.add(FRYING_PAN)
-        survivor.equipment.add(KATANA)
-        survivor.equipment.add(PISTOL)
-        survivor.equipment.add(BOTTLED_WATER)
-
-        survivor.equipment.inHands.content shouldBeEqual listOf(BASEBALL_BAT, FRYING_PAN)
-        survivor.equipment.inReserve.content shouldBeEqual listOf(KATANA, PISTOL, BOTTLED_WATER)
-
-    }
-
 }

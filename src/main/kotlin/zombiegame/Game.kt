972e4838bc1e9f70320fe90a1657ebd924934a8e
vomtee <v85ngm@gmail.com>
2024-09-24 19:51:39 +0200

    Implement "A Game can have Survivors added to it at any time"

--- src/main/kotlin/zombiegame/Game.kt
+++ src/main/kotlin/zombiegame/Game.kt
@@ -3,9 +3,11 @@ package zombiegame
 import Survivor
 
 class Game {
+    private val nameToSurvivor = mutableMapOf<String, Survivor>()
     val amountOfSurvivors
-        get() = 0
+        get() = nameToSurvivor.size
 
     fun add(survivor: Survivor) {
+        nameToSurvivor[survivor.name] = survivor
     }
 }

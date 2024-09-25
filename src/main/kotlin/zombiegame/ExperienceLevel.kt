a8b27f80a72a65beff9649e9a67fbe5a1c400232
vomtee <v85ngm@gmail.com>
2024-09-25 19:37:36 +0200

    Add test "more than 6 experience gives level yellow"

--- src/main/kotlin/zombiegame/ExperienceLevel.kt
+++ src/main/kotlin/zombiegame/ExperienceLevel.kt
@@ -3,11 +3,14 @@ package zombiegame
 class ExperienceLevel {
     val experience: Int = 0
     val level: LevelType = LevelType.Blue
+
+    fun incremenentExperience() {
+    }
 }
 
 enum class LevelType {
     Blue,
-//    Yellow,
+    Yellow,
 //    Orange,
 //    Red
 }
--- src/test/kotlin/zombiegame/ExperienceLevelTest.kt
+++ src/test/kotlin/zombiegame/ExperienceLevelTest.kt
@@ -1,3 +1,20 @@
 package zombiegame
 
-class ExperienceLevelTest
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import zombiegame.LevelType.Yellow
+
+class ExperienceLevelTest {
+    val experienceLevel = ExperienceLevel(
+
+    )
+
+    @Test
+    fun `more than 6 experience gives level yellow`() {
+        repeat(7) {
+            experienceLevel.incremenentExperience()
+        }
+
+        experienceLevel.level shouldBe Yellow
+    }
+}

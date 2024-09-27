503048eeeb37a7e3e9bc29f89dbc9df3279f5128
vomtee <v85ngm@gmail.com>
2024-09-27 19:45:05 +0200

    Score can detect level changeˆ

--- src/main/kotlin/zombiegame/Score.kt
+++ src/main/kotlin/zombiegame/Score.kt
@@ -3,17 +3,26 @@ package zombiegame
 class Score {
     var experience: Int = 0
         private set
+    var previousLevel: LevelType = LevelType.Blue
+        private set
     var level: LevelType = LevelType.Blue
         private set
 
     fun incrementExperience() {
+        val oldLevel = level
         experience++
         when {
             experience > 42 -> level = LevelType.Red
             experience > 18 -> level = LevelType.Orange
             experience > 6 -> level = LevelType.Yellow
         }
+
+        if (level > oldLevel) {
+            previousLevel = oldLevel
+        }
     }
+
+    fun levelHasGoneUp() : Boolean = level > previousLevel
 }
 
 enum class LevelType {
--- src/test/kotlin/zombiegame/ScoreTest.kt
+++ src/test/kotlin/zombiegame/ScoreTest.kt
@@ -1,7 +1,9 @@
 package zombiegame
 
+import io.kotest.matchers.comparables.shouldBeGreaterThan
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import zombiegame.LevelType.Blue
 import zombiegame.LevelType.Orange
 import zombiegame.LevelType.Red
 import zombiegame.LevelType.Yellow
@@ -10,10 +12,19 @@ class ScoreTest {
     val score = Score()
 
     @Test
+    fun `previous and current level are initially the same`() {
+        score.level shouldBe score.previousLevel
+        score.level shouldBe Blue
+    }
+
+    @Test
     fun `more than 6 experience gives level Yellow`() {
         repeat(7) {
             score.incrementExperience()
         }
+
+        score.level shouldBeGreaterThan score.previousLevel
+        score.levelHasGoneUp() shouldBe true
         score.level shouldBe Yellow
     }
 

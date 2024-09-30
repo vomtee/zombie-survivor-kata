9c06563b74bbce6f54fe4336ce07291a3789a3d9
vomtee <v85ngm@gmail.com>
2024-09-30 20:53:43 +0200

    Implement skill trees for orange and red level

--- src/main/kotlin/zombiegame/SkillTree.kt
+++ src/main/kotlin/zombiegame/SkillTree.kt
@@ -1,7 +1,14 @@
 package zombiegame
 
+import zombiegame.LevelType.Orange
+import zombiegame.LevelType.Red
 import zombiegame.LevelType.Yellow
+import zombiegame.SkillType.HOARD
 import zombiegame.SkillType.PLUS_ONE_ACTION
+import zombiegame.SkillType.PLUS_ONE_DIE_MELEE
+import zombiegame.SkillType.PLUS_ONE_DIE_RANGED
+import zombiegame.SkillType.PLUS_ONE_FREE_MOVE
+import zombiegame.SkillType.SNIPER
 
 typealias SkillList = List<SkillType>
 
@@ -10,17 +17,18 @@ class SkillTree(levelToSkills: MutableMap<LevelType, SkillList> = mutableMapOf()
 
     init {
         levelToSkills[Yellow] = listOf(PLUS_ONE_ACTION)
-
+        levelToSkills[Orange] = listOf(PLUS_ONE_DIE_RANGED, PLUS_ONE_DIE_MELEE)
+        levelToSkills[Red] = listOf(PLUS_ONE_FREE_MOVE, HOARD, SNIPER)
     }
 }
 
 enum class SkillType(description: String) {
-    PLUS_ONE_ACTION("+1 Action");
-//    PLUS_ONE_DIE_RANGED("+1 Die (Ranged)"),
-//    PLUS_ONE_DIE_MELEE("+1 Die (Melee)"),
-//    PLUS_ONE_FREE_MOVE("+1 Free Move Action"),
-//    HOARD("Hoard"),
-//    SNIPER("Sniper"),
+    PLUS_ONE_ACTION("+1 Action"),
+    PLUS_ONE_DIE_RANGED("+1 Die (Ranged)"),
+    PLUS_ONE_DIE_MELEE("+1 Die (Melee)"),
+    PLUS_ONE_FREE_MOVE("+1 Free Move Action"),
+    HOARD("Hoard"),
+    SNIPER("Sniper");
 //    TOUGH("Tough");
 
     var locked: Boolean = true
--- src/test/kotlin/zombiegame/SkillTreeTest.kt
+++ src/test/kotlin/zombiegame/SkillTreeTest.kt
@@ -1,7 +1,10 @@
 package zombiegame
 
 import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import zombiegame.LevelType.Orange
+import zombiegame.LevelType.Red
 import zombiegame.LevelType.Yellow
 
 class SkillTreeTest {
@@ -9,6 +12,25 @@ class SkillTreeTest {
 
     @Test
     fun `skill tree has one locked skill at level yellow`() {
-        skillTree[Yellow]?.shouldBeEqual(listOf(SkillType.PLUS_ONE_ACTION))
+        skillTree[Yellow]?.let {
+            it.shouldBeEqual(listOf(SkillType.PLUS_ONE_ACTION))
+            it.first().locked shouldBe true
+        }
+    }
+
+    @Test
+    fun `skill tree has two skills at level orange`() {
+        skillTree[Orange]?.let {
+            it.size shouldBe 2
+            it.filter { !it.locked } shouldBe emptyList()
+        }
+    }
+
+    @Test
+    fun `skill tree has three skills at level red`() {
+        skillTree[Red]?.let {
+            it.size shouldBe 3
+            it.filter { !it.locked } shouldBe emptyList()
+        }
     }
 }

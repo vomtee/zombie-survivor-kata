99a7cf8f40ebf9a27ef20725d99f4f36d55bf043
vomtee <v85ngm@gmail.com>
2024-10-18 21:14:37 +0200

    Simplify advancement calculationˆ

--- src/main/kotlin/zombiegame/Advancement.kt
+++ src/main/kotlin/zombiegame/Advancement.kt
@@ -1,13 +1,5 @@
 package zombiegame
 
 fun advancement(experience: Int): Int {
-    if (experience > 2 * LevelType.Red.experience) {
-        return 3
-    }
-
-    if (experience > LevelType.Red.experience) {
-        return 2
-    }
-
-    return 1
+    return (experience / LevelType.Red.experience) + 1
 }

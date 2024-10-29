c391880b80f7f5b83646c9c0d89dbdeec61f9ee5
vomtee <v85ngm@gmail.com>
2024-10-29 20:20:57 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.0.10"
+}
+
+group = "de.v85n"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
